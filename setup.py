4d3021c884129b20daefa0b54ab536693b8e55fe
geneb <geneb@deltasoft.com>
2016-12-08 19:19:50 -0800

    Initial version for public consumption.  Don't mind me, I'll be hiding under my desk.

--- setup.py
+++ setup.py
@@ -14,11 +14,11 @@ plugin_package = "delta_cal"
 plugin_name = "Delta-Auto-Calibration"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.0.5"
+plugin_version = "0.1.0"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """Delta Auto-Calibration for the Rostock MAX v3"""
+plugin_description = """Delta Auto-Calibration for SeeMeCNC Delta Printers"""
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "Gene Buckle"
